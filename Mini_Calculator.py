814a5ce7134d2e3be3dc09bca984fafe910ea555
hminh1231 <102978081+hminh1231@users.noreply.github.com>
2025-05-13 11:00:20 -0700

    Add files via upload

---
+++ Mini_Calculator.py
@@ -0,0 +1,87 @@
+class Calculator:
+    def print_menu(self):
+        '''This function will print out menu of calculator'''
+        print('Máy tính thu nhỏ')
+        print('1. Phép cộng')
+        print('2. Phép trừ')
+        print('3. Phép nhân')
+        print('4. Phép chia')
+        print('5. Thoát ứng dụng')
+        
+    def sum(self,num1,num2):
+        '''This function will return sum of 2 numbers'''
+        return num1 + num2
+    
+    def minus(self,num1,num2):
+        '''This function will return minus result of 2 numbers'''
+        return num1 - num2
+    
+    def multiply(self,num1,num2):
+        '''This function will return multiply result of 2 numbers'''
+        return num1 * num2
+    
+    def divide(self,num1,num2):
+        '''This function will return division result of 2 numbers and raise error if divive by 2'''
+        try:
+            result = num1 / num2
+            return result
+        except ZeroDivisionError:
+            return "Can't divide by zero"
+                
+    
+    def options(self):
+        '''This function will: 
+        _ Take user input
+        _ Raise Error if input is not a number or out of range
+        _ Call the function depends on user input'''
+        while True:
+            while True:
+                try:
+                    option = int(input('Lựa chọn của bạn là gì: '))
+                    break
+                except ValueError:
+                    print('Lựa chọn của bạn không hợp lệ, vui lòng chọn lại')
+                    continue
+            if option == 5:
+                exit = False
+                break
+            while True:
+                try:
+                    num1 = int(input('Mời bạn nhập số đầu tiên: '))
+                    break
+                except:
+                    print('Lựa chọn của bạn không phù hợp, vui lòng chọn lại')
+                    continue
+            while True:
+                try:
+                    num2 = int(input('Mời bạn nhập số thứ hai: '))
+                    break
+                except ValueError:
+                    print('Lựa chọn của bạn không phù hợp, vui lòng chọn lại')
+                    continue
+            if option == 1:
+                print(self.sum(num1,num2))
+                print('')
+            elif option == 2:
+                print(self.minus(num1,num2))
+                print('')
+            elif option == 3:
+                print(self.multiply(num1,num2))
+                print('')
+            elif option == 4:
+                print(self.divide(num1,num2))
+                print('')
+            else:
+                print('Vui lòng chọn số từ 1-5')
+
+            
+    def display(self):
+        '''This function will display everything, call this function'''
+        while True:
+            self.print_menu()
+            if not self.options():
+                break
+            
+math = Calculator()
+math.display()
+            
